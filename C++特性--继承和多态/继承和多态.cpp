d233fab11c90ccc1018687d1532b963619470afb
NeilZhy <NeilZhy@163.com>
2017-09-18 17:40:31 +0800

    子类的六个默认成员函数

--- C++特性--继承和多态/继承和多态.cpp
+++ C++特性--继承和多态/继承和多态.cpp
@@ -1,39 +1,108 @@
 #include<iostream>
 using namespace std;
+//class A
+//{
+//public:
+//	A(int a)
+//		:_a(a)
+//	{}
+//private:
+//	void ShowA()
+//	{
+//		cout << "I am A." << endl;
+//	}
+//	void ShowAA()
+//	{
+//		ShowA();
+//	}
+//public:
+//	int _a;
+//};
+//
+//
+//class B : public A
+//{
+//public:
+//	B(int a)
+//		:A(a)
+//		, _b(a)
+//	{
+//	}
+//	void ShowB()
+//	{
+//		cout << "B" << endl;
+//		//ShowA();
+//	}
+//public:
+//	int _b;
+//};
+
 class A
 {
 public:
-	A(int a)
+	A(int a = 0)
 		:_a(a)
-	{}
+	{
+	}
+
+	A(const A& a)
+	{
+		_a = a._a;
+	}
+
+	A& operator=(const A& a)
+	{
+		if (this == &a)
+		{
+		}
+		else
+		{
+			_a = a._a;
+		}
+		return *this;
+	}
+
+	~A()
+	{
+		cout << "~A()" << endl;
+	}
+
 private:
-	void ShowA()
-	{
-		cout << "I am A." << endl;
-	}
-	void ShowAA()
-	{
-		ShowA();
-	}
-public:
 	int _a;
 };
-	
 
 class B : public A
 {
 public:
-	B(int a)
-		:_b(0)
-		, A(a)
+	B(int a = 0,int b = 1)
+		:A(a)
+		,_b(b)
 	{
 	}
-	void ShowB()
+
+	B(const B& b)
+		:A(b)
+	     ,_b(b._b)
 	{
-		cout << "B" << endl;
-		//ShowA();
 	}
-public:
+
+	B& operator=(const B& b)
+	{
+		if (this != &b)
+		{
+			A::operator=(b);
+			_b = b._b;
+
+		}
+		return *this;
+	}
+
+	~B()
+	{
+		cout << "~B()"<<endl;
+	}
+
+private:
 	int _b;
 };
 
@@ -41,7 +110,8 @@ int main()
 {
 	A a(0);
 	B b(0);
-	a = b;
+	/*B c(0);
+	b = c;*/
 	return 0;
 
 }
